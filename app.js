38e8671e0da51cc2b496d1fc4ecf4675c119a4ad
Alish Bista <bistace321@gmail.com>
2023-08-31 15:55:32 +0545

    feat: mongo db inititated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 // async errors
 const express = require('express');
 const app = express();
-
+const connectDB = require('./db/connect')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require("./middleware/error-handler")
 
@@ -23,6 +23,7 @@ const port = process.env.PORT || 3000
 
 const start = async() =>{
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,console.log(`Server is listening on port ${port}...`))
     } catch (error) {
         console.log(error)
