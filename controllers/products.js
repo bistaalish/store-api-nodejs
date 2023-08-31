b7b4f3d19602e28fb0d4349edbf8b92426ba70f0
Alish Bista <bistace321@gmail.com>
2023-08-31 18:28:23 +0545

    feat: created search,sort, limit and page

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,44 @@
-
+const Product = require('../models/product');
 
 const getAllProductsStatic = async (req,res) => {
-   res.status(200).json({msg: 'product testing route'}) 
+   const products = await Product.find({featured:true})
+   res.status(200).json(products) 
 }
 
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg: "product testing route"})
+    const {featured,name,sort,company,fields} = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === 'true' ? true :false
+    }
+    if(name){
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+    if(company) {
+        queryObject.company = {$regex: company, $options: 'i'}
+    }
+    let result = Product.find(queryObject)
+    // sort
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+    if(fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+    
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit;
+    result = result.skip(skip).limit(limit)
+    
+    const products = await result
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
