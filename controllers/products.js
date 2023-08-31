b3e7089ad40280cacdea618c1a9e71b29a166dd0
Alish Bista <bistace321@gmail.com>
2023-08-31 18:47:23 +0545

    feat: completed product controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,16 @@
 const Product = require('../models/product');
 
-const getAllProductsStatic = async (req,res) => {
-   const products = await Product.find({featured:true})
-   res.status(200).json(products) 
-}
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({ price: { $gt: 30 } })
+      .sort('price')
+      .select('name price');
+  
+    res.status(200).json({ products, nbHits: products.length });
+  };
+  
 
 const getAllProducts = async (req,res) => {
-    const {featured,name,sort,company,fields} = req.query
+    const {featured,name,sort,company,fields,numericFilters} = req.query
     const queryObject = {}
 
     if(featured){
@@ -18,6 +22,28 @@ const getAllProducts = async (req,res) => {
     if(company) {
         queryObject.company = {$regex: company, $options: 'i'}
     }
+    if (numericFilters) {
+        const operatorMap = {
+          '>': '$gt',
+          '>=': '$gte',
+          '=': '$eq',
+          '<': '$lt',
+          '<=': '$lte',
+        };
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+        let filters = numericFilters.replace(
+          regEx,
+          (match) => `-${operatorMap[match]}-`
+        );
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+          const [field, operator, value] = item.split('-');
+          if (options.includes(field)) {
+            queryObject[field] = { [operator]: Number(value) };
+          }
+        });
+      }
+    
     let result = Product.find(queryObject)
     // sort
     if(sort){
